5d03d20d523574e66e59f762b32175c4756ae4b4
Melozzo <carmecamma1995@gmail.com>
2023-11-23 16:43:55 +0100

    [adjust]: disposition header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,20 @@
 <header class="fixed">
-    <div class="logo">
-        <img src="/public/img/boolean-logo.png" alt="logo">
-    </div>
 
     <div class="menu-pages">
-
         <ul class=" list-unstyled d-flex">
             <li class="mx-2"><a href="/">home</a></li>
             <li class="mx-2"><a href="about">about</a></li>
             <li class="mx-2"><a href="contacts">contacts</a></li>
         </ul>
+    </div>
+
+    <div class="logo">
+        <img src="/public/img/boolean-logo.png" alt="logo">
+    </div>
+
+    <div class="icons-menu">
+        <i class="fa-regular fa-user"></i>
+        <i class="fa-regular fa-heart"></i>
+        <i class="fa-solid fa-bag-shopping"></i>
     </div>
 </header>
